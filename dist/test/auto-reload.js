59ca5c8af19402315f6786a7585801ede2ce2c61
Bernie Sumption <bernie@berniecode.com>
2015-03-17 15:21:22 +0000

    New production build

---
+++ dist/test/auto-reload.js
@@ -0,0 +1,59 @@
+(function() {
+    if (document.location.port != 3333) {
+        // don't attempt websocket magic if we're not being served through Brunch
+        return;
+    }
+    var WebSocket = window.WebSocket || window.MozWebSocket;
+    var br = window.brunch = (window.brunch || {});
+    var ar = br['auto-reload'] = (br['auto-reload'] || {});
+    if (!WebSocket || ar.disabled) return;
+
+    var cacheBuster = function(url){
+        var date = Math.round(Date.now() / 1000).toString();
+        url = url.replace(/(\&|\\?)cacheBuster=\d*/, '');
+        return url + (url.indexOf('?') >= 0 ? '&' : '?') +'cacheBuster=' + date;
+    };
+
+    var reloaders = {
+        page: function(){
+            window.location.reload(true);
+        },
+
+        stylesheet: function(){
+            [].slice
+                .call(document.querySelectorAll('link[rel="stylesheet"]'))
+                .filter(function(link){
+                    return (link != null && link.href != null);
+                })
+                .forEach(function(link) {
+                    link.href = cacheBuster(link.href);
+                });
+
+            // hack to force page repaint
+            var el = document.body;
+            var bodyDisplay = el.style.display || 'block';
+            el.style.display = 'none';
+            el.offsetHeight;
+            el.style.display = bodyDisplay;
+        }
+    };
+    var port = ar.port || 9485;
+    var host = br.server || window.location.hostname || 'localhost';
+
+    var connect = function(){
+        var connection = new WebSocket('ws://' + host + ':' + port);
+        connection.onmessage = function(event){
+            if (ar.disabled) return;
+            var message = event.data;
+            var reloader = reloaders[message] || reloaders.page;
+            reloader();
+        };
+        connection.onerror = function(){
+            if (connection.readyState) connection.close();
+        };
+        connection.onclose = function(){
+            window.setTimeout(connect, 1000);
+        };
+    };
+    connect();
+})();
